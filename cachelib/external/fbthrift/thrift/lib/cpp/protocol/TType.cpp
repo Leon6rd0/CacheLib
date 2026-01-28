e5d7c845c61f402e881670da02404ae34d5512f9
namor <ernieworks@163.com>
2026-01-28 14:59:46 +0800

    Fix: fbthrift fix of fmt error

--- cachelib/external/fbthrift/thrift/lib/cpp/protocol/TType.cpp
+++ cachelib/external/fbthrift/thrift/lib/cpp/protocol/TType.cpp
@@ -13,10 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-#include <thrift/lib/cpp/protocol/TType.h>
-
 #include <fmt/format.h>
+#include <thrift/lib/cpp/protocol/TType.h>
 
 namespace apache::thrift::protocol {
 
